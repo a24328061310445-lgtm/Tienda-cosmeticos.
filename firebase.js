f8bd7d22064e10343ef99da10e5f03057efa772d
a24328061310445-lgtm <a24328061310445@cetis131.edu.mx>
2026-03-26 00:01:57 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,15 @@
 // firebase.js
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // Esto es para usar la base de datos
+import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc } from "firebase/firestore";
 
-// Tu configuración de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyD3Cms2PyOYMCIYVgv_e0YtBHyZSqi6AOA",
-  authDomain: "tienda-cosmeticos-c0e38.firebaseapp.com",
-  projectId: "tienda-cosmeticos-c0e38",
-  storageBucket: "tienda-cosmeticos-c0e38.firebasestorage.app",
-  messagingSenderId: "222333125235",
-  appId: "1:222333125235:web:b4fef9bcf45076c7a96006"
+  apiKey: "TU-APIKEY",
+  authDomain: "TU-PROYECTO.firebaseapp.com",
+  projectId: "TU-PROYECTO",
+  storageBucket: "TU-PROYECTO.appspot.com",
+  messagingSenderId: "TU-SENDER-ID",
+  appId: "TU-APP-ID"
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-
-// Exportar Firestore para usarlo en otros archivos
 export const db = getFirestore(app);
